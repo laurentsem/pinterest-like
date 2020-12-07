1e46f3a6d74a02c561a73d5c11c1119a746c7beb
houssam <hlaghzil@gmail.com>
2020-12-07 11:59:26 +0100

    initial commit

---
+++ src/composent/js/App.js
@@ -0,0 +1,14 @@
+import logo from '../../logo.svg';
+import '../../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pinterest</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
