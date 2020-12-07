6cf0f26dd330f176c9936d154801d05fe8faf70d
houssam <hlaghzil@gmail.com>
2020-12-07 13:38:10 +0100

    intégration bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
 import './css/App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Pinterest</h1>
+        <span>create'n share</span>
       </header>
+        <button type="button" className="btn btn-outline-danger">connection</button>
     </div>
   );
 }
