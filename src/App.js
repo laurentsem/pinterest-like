cdcf971cb4c4bf1d4100a86aca0dc5afe6962393
laurentsem <laurent.sem@edu.itescia.Fr>
2020-12-07 16:07:04 +0100

    Separate components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './assets/css/App.css';
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <header className="App-header">
         <h1>Pinterest</h1>
         <span>create'n share</span>
       </header>
         <button type="button" className="btn btn-outline-danger">connection</button>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer/footer.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                2020 - Coding Factory by ESIEE-IT
+            </div>
+        );
+    }
+}
+
+export default Footer;
