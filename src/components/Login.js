821b73737b847131952acad6aa31514eb75403dd
houssam <hlaghzil@gmail.com>
2020-12-08 11:15:12 +0100

    gitignore + scss + css + login + nav bar

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,33 @@
 // Login
 // Page de connexion
+import"../assets/css/login.css"
 
 import React, { Component } from 'react';
 
 class ProfilePage extends Component {
     render() {
         return (
-        <div>
-            <h2>Connectez-vous !</h2>
-            <form>
-                <input type="text" placeholder="Pseudo"></input>
-                <input type="text" placeholder="Mot de passe"></input>
-                <button type="button">Se connecter</button>
-            </form>
-        </div>    
+            <div class="login-box">
+                <h2>Connectez-vous !</h2>
+                <form>
+
+                <div class="user-box">
+                <input type="text" name="" required="" placeholder="Pseudo"/>
+                <input type="password" name="" required="" placeholder="Mot de passe"/>
+                    <a href="#">
+                        <span/>
+                        <span/>
+                        <span/>
+                        <span/>
+                        <button type="button">Se connecter</button>
+                        Se connecter
+                    </a>
+
+        </div>
+                </form>
+
+            </div>
+
         )
     }
 }
