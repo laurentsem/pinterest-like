095f85d81833123e9227814f970f459221b84ccd
Hugo <hugo.monnerie@edu.itescia.fr>
2020-12-08 17:49:30 +0100

    implement widget upload image

--- src/cloudinary/Upload.js
+++ src/cloudinary/Upload.js
@@ -1,11 +1,9 @@
-import 'cloudinary'
+import React from 'react'
+
 /*
-cloudname : dp2k3zmzy
+id: hugo.monnerie@edu.itescia.fr
+cloudname: dp2k3zmzy
+api_key: 426185941939667
+api_secret: HN2_m2UKjVhfphZct8qfZcC8GPM
 mdp : f#na7YxyqYKT?#9
 */
-
-cloudinary.config ({
-    cloud_name : 'dp2k3zmzy' ,
-    api_key : '426185941939667' ,
-    api_secret : 'HN2_m2UKjVhfphZct8qfZcC8GPM'
-});
--- src/components/test.js
+++ src/components/test.js
@@ -1,46 +1,30 @@
-import React, { useState } from 'react'
+import React, {Component} from 'react';
+import {Image, CloudinaryContext, Transformation} from 'cloudinary-react';
+import {WidgetLoader, Widget} from 'react-cloudinary-upload-widget'
 
-import '../assets/css/App.css'
-
-function App() {
-    const [image, setImage] = useState('')
-    const [loading, setLoading] = useState(false)
-
-    const uploadImage = async e => {
-        const files = e.target.files
-        const data = new FormData()
-        data.append('file', files[0])
-        data.append('upload_preset', 'darwin')
-        setLoading(true)
-        const res = await fetch(
-            '	https://api.cloudinary.com/v1_1/dihifeicm/image/upload',
-            {
-                method: 'POST',
-                body: data
-            }
+class test extends Component {
+    render() {
+        return (
+            <div>
+                <WidgetLoader/>
+                <Widget
+                    sources={['local', 'url', 'camera']} // set the sources available for uploading -> by default
+                    resourceType={'image'} // optionally set with 'auto', 'image', 'video' or 'raw' -> default = 'auto'
+                    cloudName={'dp2k3zmzy'} // your cloudinary account cloud name.
+                    uploadPreset={'preset_unsigned'} // check that an upload preset exists and check mode is signed or unisgned
+                    style={{
+                        color: 'white',
+                        border: 'none',
+                        width: '120px',
+                        backgroundColor: 'green',
+                        borderRadius: '4px',
+                        height: '25px'
+                    }} // inline styling only or style id='cloudinary_upload_button'
+                    logging={false} // logs will be provided for success and failure messages,
+                />
+            </div>
         )
-        const file = await res.json()
-
-        setImage(file.secure_url)
-        setLoading(false)
-    }
-
-    return (
-        <div className="App">
-            <h1>Upload Image</h1>
-            <input
-                type="file"
-                name="file"
-                placeholder="Upload an image"
-                onChange={uploadImage}
-            />
-            {loading ? (
-                <h3>Loading...</h3>
-            ) : (
-                <img src={image} style={{ width: '300px' }}  alt={image}/>
-            )}
-        </div>
-    )
+    };
 }
 
-export default App
+export default test;
