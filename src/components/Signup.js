78966626d2febd28f353ffae62fcf02940ae5738
houssam <hlaghzil@gmail.com>
2020-12-08 11:50:43 +0100

    all css front interface

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,21 +1,26 @@
 // Signup
 // Page d'inscription
+import "../assets/css/singin.scss"
 
 import React, { Component } from 'react';
 
 class Signup extends Component {
     render() {
         return (
-        <div>
-            <h2>Join the club</h2>
             <form>
-                <input type="text" placeholder="Pseudo"></input>
-                <input type="email" placeholder="Email"></input>
-                <input type="password" placeholder="Mot de passe"></input>
-                <input type="password" placeholder="Confirmer le mot de passe"></input>
-                <button type="button">Se connecter</button>
+            <div class="segment">
+            <h2>Join the club</h2>
+            </div>
+        <div>
+            <label>
+                <input type="text" placeholder="Pseudo"/>
+                <input type="email" placeholder="Email"/>
+                <input type="password" placeholder="Mot de passe"/>
+                <input type="password" placeholder="Confirmer le mot de passe"/>
+                <button className="red" type="button"><i className="icon ion-md-lock"/> Log in</button>
+            </label>
+        </div>
             </form>
-        </div>    
         )
     }
 }
