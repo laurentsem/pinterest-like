173f532445650a388375a9d5db62f7b4ff77d79f
laurentsem <laurent.sem@edu.itescia.Fr>
2020-12-08 10:13:10 +0100

    Update gitignore

---
+++ src/server/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/firebase-app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+class Firebase {
+    constructor() {
+      firebase.initializeApp(firebaseConfig);
+    }
+  }
+   
+export default Firebase;
