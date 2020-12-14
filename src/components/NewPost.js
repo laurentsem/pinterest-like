f91b93531852004df8cad49b5381824dbd1375fe
Hugo <hugo.monnerie@edu.itescia.fr>
2020-12-14 10:58:00 +0100

    Axios on post form

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -5,19 +5,69 @@ import React, { Component } from 'react';
 import { Widget, WidgetLoader } from "react-cloudinary-upload-widget";
 import firebase from 'firebase/app';
 import { getData } from '../server/functions';
+import axios from 'axios';
 const db = firebase.firestore();
 
 
 class NewPost extends Component {
-    
+
+    state = {
+        title: '',
+        description: '',
+        imageURL: ''
+    }
+
+    titleOnChange = e => {
+        this.setState({
+            title: e.target.value,
+            description: e.target.value,
+            imageURL: e.target.value
+        })
+    }
+
+    descriptionOnChange = e => {
+        this.setState({
+            description: e.target.value
+        })
+    }
+
+    imageURLOnChange = e => {
+        this.setState({
+            imageURL: e.target.value
+        })
+    }
+
+
+    handleSubmit = e => {
+        e.preventDefault();
+
+        const data = {
+            title : this.state.title,
+            description: this.state.description,
+            imageURL: this.state.imageURL
+        };
+        axios.post("http://localhost:5000/posts", data)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
+    }
+
      render() {
         return (
         <div>
             <h2>Create new Post</h2>
-            <form>
-                <input type="text" placeholder="Nom"></input>
-                <input type="text" placeholder="Description"></input>
-                <input type="text" placeholder="Lien URL de la photo"></input>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"
+                       placeholder="Nom" value={this.state.title}
+                       onChange={this.titleOnChange}
+                />
+                <input type="text"
+                       placeholder="Description" value={this.state.description}
+                       onChange={this.descriptionOnChange}
+                />
+                <input type="text"
+                       placeholder="Lien URL de la photo" value={this.state.imageURL}
+                       onChange={this.imageURLOnChange}
+                />
                 <button type="submit">Submit</button>
             </form>
 
@@ -40,7 +90,7 @@ class NewPost extends Component {
                     }} // inline styling only or style id='cloudinary_upload_button'
                     logging={false} // logs will be provided for success and failure messages,
                 />
-        </div>    
+        </div>
         )
     }
 }
