f7b45b190eb8f71e3264d039e5a8dcb84ef4e67b
houssam <hlaghzil@gmail.com>
2020-12-14 14:58:56 +0100

    click image
    a relier au back on doit recuperer le nom de l'image sur la quel on clique

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@
 
 import React, {Component} from 'react';
 import axios from 'axios';
+import $ from 'jquery';
 import {Image, CloudinaryContext, Transformation} from 'cloudinary-react';
 import "../assets/css/homeImage.scss"
 import "../assets/jsCSS/jsCSS"
@@ -39,9 +40,10 @@ class Home extends Component {
                                             <div className="container gal-container">
                                                 <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
                                                     <div className="box">
+                                                        <a href="http://localhost:63343/pinterest-like/src/components/pageImage.html?_ijt=558n6ec8kaeu0f31u2ab2co7pq">
                                                         <Image publicId="coding_groupe_siwt8c"><Transformation
                                                             gravity="east" crop="fill" width="400"
-                                                            height="400"/></Image>
+                                                            height="400"/></Image></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -56,9 +58,10 @@ class Home extends Component {
                                             <div className="container gal-container">
                                                 <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
                                                     <div className="box">
+                                                        <a href="http://localhost:63343/pinterest-like/src/components/pageImage.html?_ijt=558n6ec8kaeu0f31u2ab2co7pq">
                                                         <Image publicId="sample"><Transformation gravity="east"
                                                                                                  crop="fill" width="400"
-                                                                                                 height="400"/></Image>
+                                                                                                 height="400"/></Image></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -81,9 +84,10 @@ class Home extends Component {
                                             <div className="container gal-container">
                                                 <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
                                                     <div className="box">
+                                                        <a href="http://localhost:63343/pinterest-like/src/components/pageImage.html?_ijt=558n6ec8kaeu0f31u2ab2co7pq">
                                                         <Image publicId="coding_groupe_siwt8c"><Transformation
                                                             gravity="east" crop="fill" width="400"
-                                                            height="400"/></Image>
+                                                            height="400"/></Image></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -97,12 +101,12 @@ class Home extends Component {
                                         <CloudinaryContext cloudName="dp2k3zmzy">
                                             <div className="container gal-container">
                                                 <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
-                                                    <div className="box">
-                                                        <Image publicId="sample"><Transformation gravity="east"
-                                                                                                 crop="fill"
-                                                                                                 width="400"
-                                                                                                 height="400"/></Image>
-                                                    </div>
+                                                  <div className="box">
+                                            <a href="http://localhost:63343/pinterest-like/src/components/pageImage.html?_ijt=558n6ec8kaeu0f31u2ab2co7pq">
+                                            <Image publicId="sample"><Transformation
+                                                gravity="east" crop="fill" width="400"
+                                                height="400"/></Image></a>
+                                        </div>
                                                 </div>
                                             </div>
                                         </CloudinaryContext>
@@ -116,9 +120,10 @@ class Home extends Component {
                                             <div className="container gal-container">
                                                 <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
                                                     <div className="box">
+                                                        <a href="http://localhost:63343/pinterest-like/src/components/pageImage.html?_ijt=558n6ec8kaeu0f31u2ab2co7pq">
                                                         <Image publicId="coding_groupe_siwt8c"><Transformation
                                                             gravity="east" crop="fill" width="400"
-                                                            height="400"/></Image>
+                                                            height="400"/></Image></a>
                                                     </div>
                                                 </div>
                                             </div>
@@ -132,12 +137,12 @@ class Home extends Component {
                                         <CloudinaryContext cloudName="dp2k3zmzy">
                                             <div className="container gal-container">
                                                 <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
-                                                    <div className="box">
-                                                        <Image publicId="sample"><Transformation gravity="east"
-                                                                                                 crop="fill"
-                                                                                                 width="400"
-                                                                                                 height="400"/></Image>
-                                                    </div>
+                                                  <div className="box">
+                                            <a href="http://localhost:63343/pinterest-like/src/components/pageImage.html?_ijt=558n6ec8kaeu0f31u2ab2co7pq">
+                                            <Image publicId="sample"><Transformation
+                                                gravity="east" crop="fill" width="400"
+                                                height="400"/></Image></a>
+                                        </div>
                                                 </div>
                                             </div>
                                         </CloudinaryContext>
@@ -145,11 +150,28 @@ class Home extends Component {
                                 </div>
                             </article>
                         </div>
-
                         <div class="showmore"><a><h4>show more</h4></a></div>
-
                     </div>
                 </div>
+                <article className="location-listing">
+                    <div className="location-image">
+                        <a href="#">
+                            <CloudinaryContext cloudName="dp2k3zmzy">
+                                <div className="container gal-container">
+                                    <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
+                                        <div className="box">
+                                            <a href="http://localhost:63343/pinterest-like/src/components/pageImage.html?_ijt=558n6ec8kaeu0f31u2ab2co7pq">
+                                            <Image publicId="sample"><Transformation
+                                                gravity="east" crop="fill" width="400"
+                                                height="400"/></Image></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </CloudinaryContext>
+                        </a>
+                    </div>
+                </article>
+
             </div>
         )
     }
