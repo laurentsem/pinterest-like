d66b62a3abca88e421895f3f6dcc1e2ce85b455e
laurentsem <laurent.sem@edu.itescia.Fr>
2020-12-14 01:18:18 +0100

    Merge comments API

---
+++ server/routes/commentRoute.js
@@ -0,0 +1,15 @@
+const {postOneComment, getOneCommentById, allCommentsByOnePost, allCommentsByOneUser, deleteOneCommentById, updateOneCommentById} = require('../controlleurs/commentsController')
+const Router = require('express').Router;
+const router = Router();
+
+router.post('/postComment', postOneComment);
+
+router.get('/comment/:id', getOneCommentById);
+
+router.get('/commentsOfPost/:id', allCommentsByOnePost);
+
+router.get('/commentsOfUser/:id', allCommentsByOneUser);
+
+router.get('/delComments/:id', deleteOneCommentById);
+
+router.post('/upComments/:id', updateOneCommentById);
