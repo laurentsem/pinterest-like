fd05f74357a5a04e8b25e589f4b5b76b47bf7b0a
houssam <hlaghzil@gmail.com>
2020-12-15 16:24:49 +0100

    affichage image & one post

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import {Image, CloudinaryContext, Transformation} from 'cloudinary-react';
 import "../assets/css/homeImage.scss"
-import "../assets/jsCSS/jsCSS"
 
 
 class Home extends Component {
@@ -23,92 +22,18 @@ class Home extends Component {
     render() {
         return (
         <div>
-            <ul>
-                {this.state.posts.map(post => <li>{post.description}</li>)}
-            </ul>
+            <div class="allbody">
+                {this.state.posts.map(post =>
                 <div className="child-page-listing">
                     <div className="grid-container">
                         <article className="location-listing">
                             <div className="location-image">
-                                <a href="#">
+                                <a href="Onepost.js">
                                     <CloudinaryContext cloudName="dp2k3zmzy">
                                         <div className="container gal-container">
                                             <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
                                                 <div className="box">
-                                                    <Image publicId="coding_groupe_siwt8c"><Transformation gravity="east" crop="fill" width="400" height="400" /></Image>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </CloudinaryContext>
-                                </a>
-                            </div>
-                        </article>
-                        <article className="location-listing">
-                            <div className="location-image">
-                                <a href="#">
-                                    <CloudinaryContext cloudName="dp2k3zmzy">
-                                        <div className="container gal-container">
-                                            <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
-                                                <div className="box">
-                                                    <Image publicId="sample"><Transformation gravity="east" crop="fill" width="400" height="400" /></Image>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </CloudinaryContext>
-                                </a>
-                            </div>
-                        </article>                        <article className="location-listing">
-                            <div className="location-image">
-                                <a href="#">
-                                    <CloudinaryContext cloudName="dp2k3zmzy">
-                                        <div className="container gal-container">
-                                            <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
-                                                <div className="box">
-                                                    <Image publicId="coding_groupe_siwt8c"><Transformation gravity="east" crop="fill" width="400" height="400" /></Image>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </CloudinaryContext>
-                                </a>
-                            </div>
-                        </article>
-                        <article className="location-listing">
-                            <div className="location-image">
-                                <a href="#">
-                                    <CloudinaryContext cloudName="dp2k3zmzy">
-                                        <div className="container gal-container">
-                                            <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
-                                                <div className="box">
-                                                    <Image publicId="sample"><Transformation gravity="east" crop="fill" width="400" height="400" /></Image>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </CloudinaryContext>
-                                </a>
-                            </div>
-                        </article>                        <article className="location-listing">
-                            <div className="location-image">
-                                <a href="#">
-                                    <CloudinaryContext cloudName="dp2k3zmzy">
-                                        <div className="container gal-container">
-                                            <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
-                                                <div className="box">
-                                                    <Image publicId="coding_groupe_siwt8c"><Transformation gravity="east" crop="fill" width="400" height="400" /></Image>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </CloudinaryContext>
-                                </a>
-                            </div>
-                        </article>
-                        <article className="location-listing">
-                            <div className="location-image">
-                                <a href="#">
-                                    <CloudinaryContext cloudName="dp2k3zmzy">
-                                        <div className="container gal-container">
-                                            <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
-                                                <div className="box">
-                                                    <Image publicId="sample"><Transformation gravity="east" crop="fill" width="400" height="400" /></Image>
+                                                    <Image publicId={post.imageURL} ><Transformation gravity="east" crop="fill" /></Image>
                                                 </div>
                                             </div>
                                         </div>
@@ -118,7 +43,9 @@ class Home extends Component {
                         </article>
                     </div>
                 </div>
+            )}
             </div>
+        </div>
         )
     }
 }
---
+++ src/components/Onepost.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import {Image, CloudinaryContext, Transformation} from 'cloudinary-react';
+
+class Onepost extends Component{
+    render() {
+        return(
+            <div>
+                {this.state.posts.map(post => <li>{post.description}</li>)}
+            </div>
+        )
+    }
+}
+
+export default Onepost;
