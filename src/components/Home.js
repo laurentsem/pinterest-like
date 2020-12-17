35d07674cf6d438e4ca474c0475b7553f5babaae
houssam <hlaghzil@gmail.com>
2020-12-17 19:09:25 +0100

    css

--- src/components/Home.js
+++ src/components/Home.js
@@ -50,16 +50,13 @@ class Home extends Component {
             <div className="allbody">
                 {this.state.posts.map(post =>
                 <div className="child-page-listing">
-                    <div className="grid-container">
                         <article className="location-listing">
                             <div className="location-image">
                                 {/*<Link to={/Onepost/${postId}}/>*/}
                                     <CloudinaryContext cloudName="dp2k3zmzy">
-                                        <div className="container gal-container">
-                                            <div className="col-md-8 col-sm-12 co-xs-12 gal-item">
                                                 <div className="box">
                                                     <Image publicId={post.imageURL} ><Transformation gravity="east" crop="fill" /></Image>
-                                                    <li>{post.title}</li>
+                                                    <li id="li">{post.title}</li>
                                                     {this.state.getId === post.userId ?
                                                         <><button onClick={() => this.DeletePost(post.docId)}>Delete post</button></>
                                                         :
@@ -67,13 +64,10 @@ class Home extends Component {
                                                         </>
                                                     }
                                                 </div>
-                                            </div>
-                                        </div>
                                     </CloudinaryContext>
                             </div>
                         </article>
                     </div>
-                </div>
             )}
             </div>
         </div>
